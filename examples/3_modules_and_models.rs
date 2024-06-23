c142b2cbfcf87b271d2027051e88b8fdaf764c47
Tucker Morgan <Tucker.bull.morgan@gmail.com>
2024-06-23 12:40:39 -0700

    Adding third example

---
+++ examples/3_modules_and_models.rs
@@ -0,0 +1,40 @@
+use poro::nn::layers::LinearLayer;
+use poro::nn::model::Sequential;
+use poro::central::*;
+use poro::nn::Model;
+
+fn main() {
+
+    // A layer is single module that can be used in a model
+    let layer = LinearLayer::new(3, 1);
+
+    // A model is a sequence of layers
+    // You can create a model by calling "into" on a vector of layers
+    let mut linear_model : Sequential = vec![layer.into()].into();
+
+    let inputs = vec![
+        vec![2.0f32, 3.0, -1.0],
+        vec![3.0, -1.0, 0.5],
+        vec![0.5, 1.0, 1.0],
+        vec![1.0, 1.0, -1.0],
+    ];
+
+    let inputs_as_tensor = Tensor::from_vec(
+        inputs.iter().flatten().map(|x| *x).collect(),
+        vec![4, 3].into(),
+    );
+
+    let outputs = vec![1.0f32, -1.0, -1.0, 1.0];
+
+    let outputs_as_tensor =
+        Tensor::from_vec(outputs.iter().map(|x| *x).collect(), vec![4, 1].into());
+
+    // This is your training loop
+    for _ in 0..50 {
+        zero_all_grads();
+        let prediction = linear_model.forward(&inputs_as_tensor);
+        let loss = (prediction - outputs_as_tensor).pow(2.0);
+        loss.backward();
+        update_parameters(-0.01);
+    }
+}
